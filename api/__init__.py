5593fc8bd3c9865a153cd04f09a8f63f571ee753
86187 <2879446029@qq.com>
2022-06-08 15:46:46 +0800

    自动化测试框架

---
+++ api/__init__.py
@@ -0,0 +1,9 @@
+
+
+
+"""
+cacheout :主要用作缓存
+导入：from import cacheout import Cache
+设置缓存：cache.set(key,value) ==> cache.set('name','张三')
+获取缓存：cache.get（key）==>  cache.get（name）
+"""
---
+++ data/uesr_mansger_data.py
@@ -0,0 +1,9 @@
+
+
+class UserManagerData():
+    add_user_data = {
+        "errno": 0 ,
+        "username":"testb05",
+        "password":"123456",
+        "new_user_name":'testb15'
+    }
